0be7401b9f4c2939b6a17f091704e961b428b373
Glopezmart <compa-dj.k2x@hotmail.com>
2016-11-08 02:27:25 -0600

    puestos

---
+++ Proyecto/src/java/Clases/Organizacion/empleados.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.Organizacion;
+
+public class empleados {
+  private String nombre;
+  private String apellidos;
+  private String direccion;
+  private String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getFnacimiento() {
+        return fnacimiento;
+    }
+
+    public String getFecha_inicio_labores() {
+        return fecha_inicio_labores;
+    }
+
+    public String getFechaingreso() {
+        return fechaingreso;
+    }
+  private String dpi;
+  private String genero;
+  private String fnacimiento;
+  private String fecha_inicio_labores;
+  private String fechaingreso;
+}
---
+++ Proyecto/src/java/Clases/Organizacion/puesto.java
@@ -0,0 +1,16 @@
+
+package Clases.Organizacion;
+public class puesto {
+  private String idpuesto;
+  private String puesto;
+
+    public String getIdpuesto() {
+        return idpuesto;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+ 
+    
+}
